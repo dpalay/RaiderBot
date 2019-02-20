4e4e91143d6a669338005c248f502fb06425a07a
dpalay <dpalay@rapodcast.net>
2019-02-19 23:27:55 -0600

    Message#delete() takes an argument for a delay!!
    WHAT?!!?!

--- Commands/terminate.js
+++ Commands/terminate.js
@@ -26,9 +26,7 @@ module.exports.run = async(client, message, activeRaids, parseArray) => {
 
                 message.channel.send(
                     `${sendlist}
-            Raid ${ID} destroyed. Thank you for using Raider!`).then((message) => {
-                    setTimeout(() => message.delete(), 2000)
-                });
+            Raid ${ID} destroyed. Thank you for using Raider!`).then((message) => message.delete(2000));
             } else {
                 message.reply("Either the raid doesn't exist, or you're not the owner.")
             }
